cfeac79fadcd0ceb8c19ccbf07cdcee3a35637e1
˜guedesgeorge˜ <george.emannuelcarvalho@gmail.com>
2024-09-09 21:49:14 -0400

    primeiro projeto

---
+++ project.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int op = -1; // incializacao da variavel
+    float resultado, n1, n2;
+
+    // Estrutura de repeticao while else
+
+    while (op != 0)
+    {
+        printf(" 0Mneu:\n");
+        printf(" 0 - sair\n");
+        printf(" 1 - somar\n");
+        printf(" 2 - subtrair");
+        printf(" Escolha a opcão: ");
+        scanf("%d", &op);
+
+
+    }
+    
+    return 0;
+}
